8e6166c921a31b641be882ade8673ae78338e44d
akilman <akilman@appdynamics.com>
2013-10-05 11:47:48 -0700

    Emacs - Adding a few options such as bracket insertion, line numbers, etc

---
+++ emacs/init.el
@@ -0,0 +1,11 @@
+;; use standard copy-paste keyboard combinations
+(cua-mode 1)
+
+;; auto-insert brackers in pairs
+(electric-pair-mode 1)
+
+;; turn on syntax coloring
+(global-font-lock-mode 1) 
+
+;; for adding line numbers; use M-x linum-mode
+(require 'linum)
