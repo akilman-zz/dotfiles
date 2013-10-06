6275ba84e7f8493d917480bd752c1cd16aae33d5
akilman <akilman@appdynamics.com>
2013-10-05 17:02:10 -0700

    Emacs - Adding markdown mode

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,6 @@
+;; load path configuration
+(add-to-list 'load-path "~/dotfiles/emacs/markdown-mode")
+
 ;; use standard copy-paste keyboard combinations
 (cua-mode 1)
 
@@ -9,3 +12,10 @@
 
 ;; for adding line numbers; use M-x linum-mode
 (require 'linum)
+
+;; markdown syntax highlighting
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
