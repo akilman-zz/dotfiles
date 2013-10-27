38234ebab9ed498fdd9308b2b6fe2431b35c21dc
akilman <akilman@appdynamics.com>
2013-10-27 09:36:37 -0700

    Emacs - Consolidate init file, add fci mode

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -15,3 +15,32 @@
 
 (require 'smart-tab)
 (global-smart-tab-mode 1)
+
+(require 'color-theme)
+
+(require 'fill-column-indicator)
+(setq fci-rule-column 90)
+(setq fci-rule-color darkblue)
+
+;; load path configuration
+(add-to-list 'load-path "~/dotfiles/emacs/markdown-mode")
+
+;; use standard copy-paste keyboard combinations
+(cua-mode 1)
+(column-number-mode 1)
+
+;; auto-insert brackers in pairs
+(electric-pair-mode 1)
+
+;; turn on syntax coloring
+(global-font-lock-mode 1) 
+
+;; for adding line numbers; use M-x linum-mode
+(require 'linum)
+
+;; markdown syntax highlighting
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
